3663a18c2ede14b239ffcf0959bc23d08a489761
Erdem <zahrakon06@gmail.com>
2022-10-22 19:25:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", downloadPackages = false, createExecutable = false } = {}) => {
+
   const path = require("path");
   const dist = path.resolve(process.cwd(), rDist);
   const main = path.resolve(process.cwd(), rMain);
@@ -7,21 +8,25 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
   const bundlePath = path.resolve(executableDir, `./${executableName.split(".").shift()}.bundle.js`)
   const distResultPath = path.resolve(dist, `./${executableName.split(".").shift()}.js`)
   const distMinPath = path.resolve(dist, `./${executableName.split(".").shift()}.min.js`)
+
   const { execAsync } = require("stuffs")
   const { readFileSync, writeFileSync, existsSync, mkdirSync, unlinkSync } = require('fs');
   const readFolder = require('recursive-readdir');
 
   let realFile = readFileSync(main, 'utf-8');
   const recImports = [...realFile.matchAll(/recursiveImport\(([^)]+)\);?/g)].map(x => [x, x[1].slice(1, -1)]);
+
   for (let i = 0; i < recImports.length; i++) {
     const statement = recImports[i][0][0];
     const path = recImports[i][1];
     const _paths = await readFolder(path)
     realFile = realFile.replace(statement, (_paths).map(x => `require('.\/${x.replaceAll("\\", "\/")}')`).join("; \n"));
   }
-  realFile = realFile.replace(/( *)recursiveImport,?( *)/g, " ");
-  realFile = realFile.replace('const { } = require("@mostfeatured/dbi");', "");
-  writeFileSync(bundlePath, realFile)
+
+  realFile = realFile.replace(/( *)recursiveImport,?( *)/g, " ").replace('const { } = require("@mostfeatured/dbi");', "");
+
+  writeFileSync(bundlePath, realFile);
+
   require('esbuild').buildSync({
     entryPoints: [bundlePath],
     bundle: true,
@@ -30,11 +35,14 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
     outfile: bundlePath,
     allowOverwrite: true,
   });
+
   const out = readFileSync(bundlePath, 'utf-8');
   unlinkSync(bundlePath);
+
   var UglifyJS = require("uglify-js");
+
   let mIn = out + "";
-  const oReqs = [...(new Set([...mIn.matchAll(/require_[a-zA-Z]+|__getOwnPropNames|__commonJS/g)].map(x => x[0])))];
+  const oReqs = [...(new Set([...mIn.matchAll(/require_[^ ]+|__getOwnPropNames|__commonJS/g)].map(x => x[0])))];
   oReqs.forEach((tReq) => mIn = mIn.replaceAll(tReq, "_" +  Math.floor(Math.random() * 1000000).toString()));
   const result = UglifyJS.minify(mIn);
   if (!existsSync(dist)) mkdirSync(dist, { recursive: true });
