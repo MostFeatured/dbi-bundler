1b9130837ebcbf9711be1510970be7f3748f35e7
Erdem <zahrakon06@gmail.com>
2022-10-24 19:24:44 +0300

    .

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
   const bundlePath = path.resolve(executableDir, `./${executableName.split(".").shift()}.bundle.js`)
   const distResultPath = path.resolve(dist, `./${executableName.split(".").shift()}.js`)
   const distMinPath = path.resolve(dist, `./${executableName.split(".").shift()}.min.js`)
+  const distCMinPath = path.resolve(dist, `./${executableName.split(".").shift()}.cmin.js`)
 
   const { execAsync, makeSureFolderExistsSync } = require("stuffs")
   const { readFileSync, writeFileSync, unlinkSync } = require('fs');
@@ -48,9 +49,9 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
   [...(new Set([...mIn.matchAll(/require_[^ ]+|__getOwnPropNames|__commonJS/g)].map(x => x[0])))]
     .forEach((tReq) => mIn = mIn.replaceAll(tReq, "_" + Math.floor(Math.random() * 1000000).toString()));
 
-  writeFileSync(distMinPath, mIn);
   const result = UglifyJS.minify(mIn, { output: { ast: true } });
-  
+  writeFileSync(distMinPath, result.code.replaceAll("\n", "\\n"));
+
   [...result.code.matchAll(/(["'])(?:(?=(\\?))\2.)*?\1/g)].forEach(([all, quato, rInner]) => {
     let nStr = quato;
     const inner = eval(`${all}`);
@@ -59,7 +60,7 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
       nStr += (
         (z = (c.charCodeAt(0)).toString(16).toUpperCase()),
         (z.length <= 1) ? z = "0" + z : null,
-        prefix = /[a-zA-Z0-9]/.test(c) ? "x" : "u0", 
+        prefix = /[a-zA-Z0-9]/.test(c) ? "x" : "u0",
         (z.length <= 2 && prefix == "u0") ? z = "0" + z : null,
         `\\${prefix}` + z
       );
@@ -76,17 +77,17 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
     result.code = result.code.replaceAll(a, rand);
   });
 
-  [...result.code.matchAll(/((["'])(?:(?=(\\?))\2.)*?\1)|((\??)\.([a-zA-Z_0-9]+))/gi)].forEach(([_,__,___,____,all,question, propName]) => {
+  [...result.code.matchAll(/((["'])(?:(?=(\\?))\2.)*?\1)|((\??)\.([a-zA-Z_0-9]+))/gi)].forEach(([_, __, ___, ____, all, question, propName]) => {
     if (!all) return;
     let nStr = `${question ? "?." : ""}[${JSON.stringify(propName)}]`
     result.code = result.code.replace(all, nStr)
   });
-  [...result.code.matchAll(/((["'])(?:(?=(\\?))\2.)*?\1)|((\??)\.([a-zA-Z_0-9]+))/gi)].forEach(([_,__,___,____,all,question, propName]) => {
+  [...result.code.matchAll(/((["'])(?:(?=(\\?))\2.)*?\1)|((\??)\.([a-zA-Z_0-9]+))/gi)].forEach(([_, __, ___, ____, all, question, propName]) => {
     if (!all) return;
     let nStr = `${question ? "?." : ""}[${JSON.stringify(propName)}]`
     result.code = result.code.replace(all, nStr)
   });
-  [...result.code.matchAll(/((["'])(?:(?=(\\?))\2.)*?\1)|(\{|\,)(([a-zA-Z0-9_]+)\:)/gi)].forEach(([_,__,___,____,_z,all, propName]) => {
+  [...result.code.matchAll(/((["'])(?:(?=(\\?))\2.)*?\1)|(\{|\,)(([a-zA-Z0-9_]+)\:)/gi)].forEach(([_, __, ___, ____, _z, all, propName]) => {
     if (!all) return;
     let nStr = `[${JSON.stringify(propName)}]:`;
     result.code = result.code.replace(all, nStr)
@@ -99,7 +100,7 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
       nStr += (
         (z = (c.charCodeAt(0)).toString(16).toUpperCase()),
         (z.length <= 1) ? z = "0" + z : null,
-        prefix = /[a-zA-Z0-9]/.test(c) ? "x" : "u0", 
+        prefix = /[a-zA-Z0-9]/.test(c) ? "x" : "u0",
         (z.length <= 2 && prefix == "u0") ? z = "0" + z : null,
         `\\${prefix}` + z
       );
@@ -113,17 +114,19 @@ const build = (async ({ dist: rDist = "./dist", main: rMain = "./index.js", down
   }
   writeFileSync(distResultPath, out);
 
-  writeFileSync(distMinPath, result.code.replaceAll("\n", "\\n"));
+  writeFileSync(distCMinPath, result.code.replaceAll("\n", "\\n"));
   const package = require(path.resolve(process.cwd(), "./package.json"));
   delete package.dependencies["uglify-js"];
   delete package.dependencies["esbuild"];
   delete package.dependencies["@mostfeatured/bundler"];
   writeFileSync(path.resolve(dist, "./package.json"), JSON.stringify(package, null, 2));
-  excludes.forEach(p => { try { 
-    const dPath = path.resolve(dist, p);
-    makeSureFolderExistsSync(path.dirname(dPath));
-    writeFileSync(dPath, readFileSync(path.resolve(process.cwd(), p), "utf-8"));
-   } catch { } });
+  excludes.forEach(p => {
+    try {
+      const dPath = path.resolve(dist, p);
+      makeSureFolderExistsSync(path.dirname(dPath));
+      writeFileSync(dPath, readFileSync(path.resolve(process.cwd(), p), "utf-8"));
+    } catch { }
+  });
   if (!downloadPackages && !createExecutable) return;
   await execAsync("npm i", dist);
   if (!createExecutable) return;
